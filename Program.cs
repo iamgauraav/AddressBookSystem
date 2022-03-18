687a46696ad04c7f4c93d7787e333901c3bdc1ec
iamgauraav <iamgauraav@gmail.com>
2022-03-18 20:54:32 +0530

    Added UC1 : To create contact in a address book

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 
 namespace AddressBook
 {
-    class Program
+    public class Program
     {
+        //UC1 to create contact in a address book
+        //declaring instance variable
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zip;
+        public string email;
+        public int phoneNumber;
+
+        //Paeameterized constructor
+        public Program(string firstName, string lastName, string address, string city, string state, string zip, int phoneNumber,string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program in AddressBookMain class on START Master Branch");
+            Console.WriteLine("Welcome to your address book");
             Console.ReadLine();
         }
     }
